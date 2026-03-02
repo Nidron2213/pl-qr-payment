c2f745b021bf860f2703a157c0ced8c4d864cb7b
Nidron2213 <michalszkolatp@gmail.com>
2026-03-02 11:05:33 +0100

    Update QrPayment.php

--- src/QrPayment.php
+++ src/QrPayment.php
@@ -2,96 +2,185 @@
 
 declare(strict_types=1);
 
-namespace IonBazan\PaymentQR\Poland;
+namespace IonBazan\PaymentQR\Poland\Tests;
 
-use Endroid\QrCode\Encoding\Encoding;
 use Endroid\QrCode\ErrorCorrectionLevel;
-use Endroid\QrCode\QrCode;
-use RuntimeException;
+use IonBazan\PaymentQR\Poland\QrPayment;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\RunInSeparateProcess;
+use PHPUnit\Framework\TestCase;
 
-class QrPayment
+class QrPaymentTest extends TestCase
 {
-    public const DELIMITER = '|';
-    public const DISALLOWED_CHARS = '/([^A-Za-z0-9 ,\.\/\\\\\-@#&\*\/¹æê³ñóœŸ¿¥ÆÊ£ÑŹÓŒąćęłńóśźżĄĆĘŁŃŚŻ¯_])/u';
-
-    /**
-     * @param string      $accountNumber Account number (26 digits)
-     * @param string      $recipient     Recipient name (max 20 characters)
-     * @param string      $title         Transfer title (max 32 characters)
-     * @param int         $amount        Amount as integer (in gr) - can be 0
-     * @param string|null $nip           Recipient NIP (10 characters)
-     * @param string      $country       Country code - the only supported is 'PL' (2 characters)
-     * @param string|null $directDebitId Customer ID for Direct Debit (max 20 characters)
-     * @param string|null $invoobillId   Invoobill ID (max 12 characters)
-     * @param string|null $reserved      Reserved (max 24 characters)
-     */
-    public function __construct(
-        public readonly string $accountNumber,
-        public readonly string $recipient,
-        public readonly string $title,
-        public readonly int $amount,
-        public readonly ?string $nip = null,
-        public readonly string $country = 'PL',
-        public readonly ?string $directDebitId = null,
-        public readonly ?string $invoobillId = null,
-        public readonly ?string $reserved = null
-    ) {
-    }
-
-    public static function fromQrString(string $qrString): self
+    public function testItGeneratesValidQrStringForExampleInput(): void
     {
-        $parts = explode(self::DELIMITER, $qrString);
+        $payment = new QrPayment(
+            '24160000035175530643314956',
+            'Testowy odbiorca',
+            'Tytuł płatności',
+            12345,
+            '5214349636',
+            'PL',
+            '11223344',
+            '990066',
+            'reserved'
+        );
 
-        return new static(
-            $parts[2],
-            $parts[4],
-            $parts[5],
-            (int) $parts[3],
-            $parts[0],
-            $parts[1],
-            $parts[6],
-            $parts[7],
-            $parts[8]
+        $this->assertSame('24160000035175530643314956', $payment->accountNumber);
+        $this->assertSame('Testowy odbiorca', $payment->recipient);
+        $this->assertSame('Tytuł płatności', $payment->title);
+        $this->assertSame(12345, $payment->amount);
+        $this->assertSame('5214349636', $payment->nip);
+        $this->assertSame('PL', $payment->country);
+        $this->assertSame('11223344', $payment->directDebitId);
+        $this->assertSame('990066', $payment->invoobillId);
+        $this->assertSame('reserved', $payment->reserved);
+
+        $this->assertSame(
+            '5214349636|PL|24160000035175530643314956|012345|Testowy odbiorca|Tytuł płatności|11223344|990066|reserved',
+            $payment->getQrString()
         );
     }
 
-    public function getQrString(): string
+    #[DataProvider('documentationExamplesProvider')]
+    public function testItGeneratesValidQrString(string $expectedResult, QrPayment $paymentQr): void
     {
-        $amount = sprintf('%06d', $this->amount);
+        $this->assertSame($expectedResult, $paymentQr->getQrString());
+        $this->assertEquals($paymentQr, QrPayment::fromQrString($expectedResult));
+    }
 
-        $parts = [
-            $this->filterVar($this->nip, 10),
-            $this->filterVar($this->country, 2),
-            $this->filterVar($this->accountNumber, 26),
-            $amount,
-            $this->filterVar($this->recipient, 20),
-            $this->filterVar($this->title, 32),
-            $this->filterVar($this->directDebitId, 20),
-            $this->filterVar($this->invoobillId, 12),
-            $this->filterVar($this->reserved, 24 - strlen($amount) + 6),
+    #[DataProvider('documentationExamplesProvider')]
+    public function testItCreatesValidObjectFromString(string $inputString, QrPayment $expectedPayment): void
+    {
+        $payment = QrPayment::fromQrString($inputString);
+        $this->assertEquals($expectedPayment, $payment);
+        $this->assertSame($inputString, $payment->getQrString());
+    }
+
+    #[RunInSeparateProcess]
+    public function testItGeneratesQrImage(): void
+    {
+        $payment = new QrPayment(
+            '24160000035175530643314956',
+            'Testowy odbiorca',
+            'Tytuł płatności',
+            12345,
+            '5214349636',
+            'PL',
+            '11223344',
+            '990066'
+        );
+
+        $qrCode = $payment->getQrCode();
+
+        $this->assertSame(
+            '5214349636|PL|24160000035175530643314956|012345|Testowy odbiorca|Tytuł płatności|11223344|990066|',
+            $qrCode->getData()
+        );
+
+        if (class_exists(ErrorCorrectionLevel\ErrorCorrectionLevelLow::class)) {
+            $correctionLevel = new ErrorCorrectionLevel\ErrorCorrectionLevelLow();
+        } else {
+            $correctionLevel = ErrorCorrectionLevel::Low;
+        }
+
+        $this->assertSame(250, $qrCode->getSize());
+        $this->assertEquals($correctionLevel, $qrCode->getErrorCorrectionLevel());
+        $this->assertSame('UTF-8', (string) $qrCode->getEncoding());
+    }
+
+    public function testItThrowsExceptionWhenEndroidIsNotInstalled(): void
+    {
+        $autoloaders = spl_autoload_functions();
+        $this->expectException(\RuntimeException::class);
+        $payment = new QrPayment(
+            '24160000035175530643314956',
+            'Testowy odbiorca',
+            'Tytuł płatności',
+            12345,
+            '5214349636',
+            'PL',
+            '11223344',
+            '990066'
+        );
+
+        array_map('spl_autoload_unregister', $autoloaders);
+
+        try {
+            $payment->getQrCode();
+        } catch (\Throwable $exception) {
+            throw $exception;
+        } finally {
+            array_map('spl_autoload_register', $autoloaders);
+        }
+    }
+
+    public function testItStripsInvalidCharactersAndTrims(): void
+    {
+        $payment = new QrPayment(
+            '|%()24160000035175530643314956_123456789',
+            '|%()Testowy odbiorca 123456789',
+            '|%()Tytuł płatności 01234567890123456789',
+            123456789,
+            '|%()5214349636_123456789',
+            '|%()PL123',
+            '|%()123456789012345678901234567890',
+            '|%() 990066990066990066',
+            '123456789012345678901234'
+        );
+
+        $this->assertSame(
+            '5214349636|PL|24160000035175530643314956|123456789|Testowy odbiorca 123|Tytuł płatności 0123456789012|12345678901234567890|990066990066|123456789012345678901',
+            $payment->getQrString()
+        );
+    }
+
+    public static function documentationExamplesProvider(): array
+    {
+        return [
+            '2D dla Odbiorcy typ 1 z kwotą płatności bez możliwości edycji' => [
+                '1234567890|PL|92124012340001567890123456|001200|Odbiorca 1|FV 1234/34/2012|||',
+                new QrPayment(
+                    '92124012340001567890123456',
+                    'Odbiorca 1',
+                    'FV 1234/34/2012',
+                    1200,
+                    '1234567890'
+                ),
+            ],
+            '2D dla Odbiorcy typ 1 z manualnie uzupełnianą kwotą przez dokonującego płatność' => [
+                '1234567890|PL|92124012340001567890123456|000000|Odbiorca 1|FV 1234/34/2012|||',
+                new QrPayment(
+                    '92124012340001567890123456',
+                    'Odbiorca 1',
+                    'FV 1234/34/2012',
+                    0,
+                    '1234567890'
+                ),
+            ],
+            '2D dla Odbiorcy typ 2 z kwotą płatności bez możliwości edycji' => [
+                '|PL|92124012340001567890123456|001200|Odbiorca 1|Przelew ekspress|||',
+                new QrPayment(
+                    '92124012340001567890123456',
+                    'Odbiorca 1',
+                    'Przelew ekspress',
+                    1200
+                ),
+            ],
+            'Full example' => [
+                '5214349636|PL|4249000050026313017364142|012345|Testowy odbiorca|Tytuł płatności|11223344|990066|reserved',
+                new QrPayment(
+                    '4249000050026313017364142',
+                    'Testowy odbiorca',
+                    'Tytuł płatności',
+                    12345,
+                    '5214349636',
+                    'PL',
+                    '11223344',
+                    '990066',
+                    'reserved'
+                ),
+            ],
         ];
-
-        return implode(self::DELIMITER, $parts);
-    }
-
-    public function getQrCode(): QrCode
-    {
-        if (!class_exists(QrCode::class)) {
-            throw new RuntimeException('Generating QR images requires endroid/qr-code');
-        }
-
-        if (method_exists(QrCode::class, 'create')) {
-            $qrCode = QrCode::create($this->getQrString());
-            $qrCode->setSize(250);
-
-            return $qrCode;
-        }
-
-        return new QrCode($this->getQrString(), new Encoding('UTF-8'), ErrorCorrectionLevel::Low, 250);
-    }
-
-    private function filterVar(?string $variable, int $length): ?string
-    {
-        return substr(trim(preg_replace(self::DISALLOWED_CHARS, '', (string) $variable)), 0, $length);
     }
 }
